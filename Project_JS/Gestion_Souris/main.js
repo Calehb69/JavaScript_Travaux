d573df74cae7fc5565e6f76640d6d1b4712b39fa
Calehb69 <isarhu@gmail.com>
2021-10-30 12:11:36 +0200

    Update_Travaux_JS

--- Project_JS/Gestion_Souris/main.js
+++ Project_JS/Gestion_Souris/main.js
@@ -1,23 +1,48 @@
 "use strict";
 
+
+/*****************
+ *  CONSTANTES
+******************/
+const button = document.querySelector("#toggle-rectangle");
+const rectangle = document.querySelector(".rectangle");
+
 /***************
- * FONCTION
+ * FONCTIONS
 ****************/
 function toggleRectangle() {
+  // La méthode .toggle() va ajouter ou supprimer la classe (tel un interrupteur).
   console.log("Ok click")
   document.querySelector(".rectangle").classList.toggle("hide");
 }
 
 function onMouseOverRectangle() {
-  const rectangle = document.querySelector("#rectangle");
-  rectangle.classList.remove("rectangle.important");
+  // La méthode .remove() va ajouter la classe.
+  rectangle.classList.add("important");
 }
+
+function onDoubleClickRectangle(){
+  rectangle.classList.toggle("good");
+  rectangle.addEventListener("dbclick", onDoubleClickRectangle);
+}
+
+function onMouseOutRectangle() {
+  // La méthode .remove() va supprimer la classes.
+    rectangle.classList.remove("important");
+  rectangle.classList.remove("good");
+}
+
 /******************
  * CODE PRINCIPAL
 *******************/
+
 /*SUR LE BOUTON 
 #toggle-rectangle-> clic -> appelle toggleRectangle*/
-
 document.querySelector("#toggle-rectangle").addEventListener("click", toggleRectangle);
 
-document.querySelector("#rectangle").addEventListener("mouseOut", onMouseOverRectangle);
+// Installation de deux gestionnaires d'évènements de survol du rectangle à la souris.
+rectangle.addEventListener("mouseover", onMouseOverRectangle);
+rectangle.addEventListener("mouseout", onMouseOutRectangle);
+
+// Installation d'un gestionnaire d'évènement double clic sur le rectangle.
+rectangle.addEventListener("dblclick", onDoubleClickRectangle);
