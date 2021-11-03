94a53d0b8affba89a18088a87f6d9af31eafbde0
Calehb69 <isarhu@gmail.com>
2021-11-03 16:49:58 +0100

    UPDATE_JavaScript_Travaux

--- Project_JS/Caroussel_Photos/js/main.js
+++ Project_JS/Caroussel_Photos/js/main.js
@@ -1,5 +1,7 @@
 "use-strict";
 /**********************************************/
+//Tableau d'objets => contient les infos utiles à la construction du carrousel (propriétées : SRC et légendes).
+let button;
 
 const PHOTOS = [
   {
@@ -34,23 +36,93 @@ const PHOTOS = [
   },
 ];
 
-
+let id = {};
 /*************
  * FONCTIONS
-**************/
-//1ere fonction que c'est comme ça qu'on doit faire désormais cependant j'ai pas tout compris...
-function refreshSlider(){
+ **************/
+function refreshSlider() {
+  //cible le figcaption qui est dans le HTML et le stocke dans la constante "figcaption"
   const figcaption = document.querySelector("#slider figcaption");
+  //cible l'image qui est dans le HTML et le stocke dans la constante "img"
   const img = document.querySelector("#slider img");
-  figcaption.textContent = PHOTOS[0].legend;
-  img.src = `images/${PHOTOS[0].src}`; //Chemin de la photo.
-  img.alt = PHOTOS[0].legend; //Légende associée à chaque image.
+  //injecte la legende (contenue dans l'objet de la photo en cours dans le tableau PHOTOS) dans le contenu textuel du figcaption
+  figcaption.textContent = PHOTOS[state.index].legend;
+  //injecte le chemin (contenu dans l'objet de la photo en cours dans le tableau PHOTOS) de l'image dans la propriété src de la balise "img"
+  img.src = `images/${PHOTOS[state.index].src}`;
+  //injecte la legende (contenue dans l'objet de la photo en cours dans le tableau PHOTOS) dans l'attribut de texte alternatif de la photo (balise "img")
+  img.alt = PHOTOS[state.index].legend;
+}
+//Cibler la "ul" afin de récupérer l'ensemble des éléménts contenus à l'intérieur et enlever sa propriété "hide" via le toggle.
+function toolbarToggle() {
+  //Alternance des classes font-awesome pour que ce soit un coup en bas / un coup à droite (rotation flèche).
+  console.log("Ok click");
+  document.querySelector(".toolbar ul").classList.toggle("hide");
+  const icon = document.querySelector("#toolbar-toggle i");
+  icon.classList.toggle("fa-arrow-right");
+  icon.classList.toggle("fa-arrow-down");
 }
 
+function next() {
+  state.index++;
+  if (state.index > PHOTOS.length - 1) state.index = 0;
+  console.log(state.index);
+  refreshSlider();
+}
+
+function previous() {
+  state.index--;
+  if (state.index < 0) state.index = PHOTOS.length - 1;
+  console.log(state.index);
+  refreshSlider();
+}
+
+function random() {
+  let nb = getRandomInteger(0, PHOTOS.length - 1);
+  while (nb === state.index) {
+    nb = getRandomInteger(0, PHOTOS.length - 1);
+  }
+  state.index = nb;
+  console.log(state.index);
+  refreshSlider();
+}
+
+function diapo() {
+  console.log(state.interval);
+  const icon = document.querySelector("#slider-toggle i");
+  icon.classList.toggle("fa-play");
+  icon.classList.toggle("fa-pause");
+
+  if (state.interval == false) {
+    state.interval = setInterval(next, 2000);
+  } else {
+    console.log("stop");
+    clearInterval(state.interval);
+    state.interval = false;
+  }
+}
 /**********************
  * CODE PRINCIPAL
-***********************/
-//Qu'est-ce à dire que ceci !!??
-document.addEventListener("DOMContentLoaded", function() {
-refreshSlider();
+ ***********************/
+//fonction anonyme qui est appelée au déclenchement de l'événement "PAGE HTML BIEN CHARGEE (DOMContentLoaded) sur la page web (document).
+//Tout le code principal en lien avec des éléments HTML doit se trouver dans cette fonction.
+//Le code ne s'execute que quand la page HTML a fini de charger.
+document.addEventListener("DOMContentLoaded", function () {
+  state = {};
+  state.index = 0;
+  state.interval = false;
+  refreshSlider();
+
+  document
+    .querySelector("#toolbar-toggle")
+    .addEventListener("click", toolbarToggle);
+
+  document.querySelector("#slider-next").addEventListener("click", next);
+
+  document
+    .querySelector("#slider-previous")
+    .addEventListener("click", previous);
+
+  document.querySelector("#slider-random").addEventListener("click", random);
+
+  document.querySelector("#slider-toggle").addEventListener("click", diapo);
 });
