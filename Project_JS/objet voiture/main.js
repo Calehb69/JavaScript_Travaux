d73771893bbe3c48750c43674adb547747546f30
Calehb69 <isarhu@gmail.com>
2021-11-05 15:54:21 +0100

    UPDATE_JavaScript_Travaux

--- Project_JS/objet voiture/main.js
+++ Project_JS/objet voiture/main.js
@@ -1,20 +1,21 @@
 "use strict";
 
 let voiture = {
-  marque: "Renault Twingo 3",
-  annee: 2014,
-  dateAchat: "Fri-Jul-31-2015",
-  passagers: ["Audrey", "Gribouille"],
+  marque : ("Renaut Twingo 3"),
+  anneeFabrication : (2014),
+  dateAchat : ("Fri-Jul-31-2015"),
+  Passager : ["Audrey", "Gribouille"],
 };
 
 document.querySelector(
   "#content"
 ).innerHTML += `<p> <strong> Information sur la voiture : </strong> <ul>
 <li> Marque : ${voiture.marque}</li>
-<li> Anné de construction : ${voiture.annee}</li>
+<li> Année de construction : ${voiture.anneeFabrication}</li>
 <li> Date d'achat : ${voiture.dateAchat}</li>
 <li> Passagers : <ol>
-<li>${voiture.passagers[0]} </li>
-<li>${voiture.passagers[1]} </li>
+<li>${voiture.Passager[0]}</li>
+<li>${voiture.Passager[1]}</li>
 </li></ol>
-</ul></p>`;
+</ul></p>
+`;
